8f07990847fbabd222b459121ac634f58582cf38
Attiyah <euphxric.melxnin@gmail.com>
2023-02-16 17:58:52 -0800

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-
+/**
+ * My first c program
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingiual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
