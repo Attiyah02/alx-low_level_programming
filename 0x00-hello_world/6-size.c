a07cfe3d6512ea408ce1443efd99c33723a963a6
Attiyah <euphxric.melxnin@gmail.com>
2023-02-19 04:27:57 -0800

    Size is not grandeur, and territory does not make a nation

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entering point
+ *
+ * Return: 0(Succesful)
+ *
+ */
+
+int main(void)
+{
+	printf("Size of char: %d byte(s) \n", sizeof(char));
+	return (0);
+}
