dc465bd21a009d4171afc03882bb74f6d5dd1319
Attiyah <euphxric.melxnin@gmail.com>
2023-02-20 19:47:04 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints numbers of single base
+ *
+ * Return: 0(Sucessful)
+ */
+
+int main(void)
+{
+	int end_at_nine;
+	for (end_at_nine = '0'; end_at_nine <= '9'; end_at_nine++)
+	{
+		putchar(end_at_nine);
+	}
+	putchar('\n');
+	return (0);
+}
