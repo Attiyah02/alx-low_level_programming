6f80f1afde5816c6bbae566aa52b3e253aeb828c
Attiyah <euphxric.melxnin@gmail.com>
2023-02-28 04:32:33 -0800

    Prints other character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2 - prints one character out of two
+ * @str: input
+ * Return: print
+ */
+
+void puts2(char *str)
+{
+	int j = 0;
+
+	for (; str[j] != '\0'; j++)
+	{
+		if ((j % 2) == 0)
+		{
+			_putchar(str[j]);
+		}
+		else
+		{
+			continue;
+		}
+		_putchar('\n');
+	}
+}
