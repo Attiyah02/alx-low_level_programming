f925d160ba1205509518315b5a92e89f32571db9
Attiyah <euphxric.melxnin@gmail.com>
2023-03-01 13:12:55 -0800

    Encodes a string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * leet - encode into 1337speak
+ * @n: input value
+ * Return: n
+ */
+
+char *leet(char *n)
+{
+	int c, d;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (c = 0; n[c] != '\0'; d++)
+	{
+		for (d = 0; d < 10; d++)
+		{
+			if (n[c] == s1[d])
+			{
+				n[c] = s2[d];
+			}
+		}
+	}
+	return (n);
+}
