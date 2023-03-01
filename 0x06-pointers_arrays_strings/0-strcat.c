78c98b01ad7d506e5f6f3e11e2a07d3c4c0081ae
Attiyah <euphxric.melxnin@gmail.com>
2023-03-01 01:58:00 -0800

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,1 +1,30 @@
-#include  
+#include "main.h"
+
+/**
+ * _strcat - concatenates two tsring
+ * @dest: input value
+ * @src: input value
+ * Return: void
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int c;
+	int t;
+
+	c = 0;
+	while (dest[c] != '\0')
+	{
+		c++;
+	}
+	t = 0;
+	while (src[t] != '\0')
+	{
+		dest[c] = src[t];
+		c++;
+		t++;
+	}
+
+	dest[c] = '\0';
+	return (dest);
+}
