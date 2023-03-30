959c5aaf186c4e8172bd562c99a481f11e3f365f
Attiyah <euphxric.melxnin@gmail.com>
2023-03-30 07:41:30 -0700

    header file

--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -5,19 +5,19 @@
 
 /**
  * struct list_s - singly linked list
- * @str: string - (it is the string being passed to malloc)
+ * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
+ *
+ * Description: singly linked list node structure
  */
 
-typedef struct list_S
+typedef struct list_s
 {
 	char *str;
 	unsigned int len;
 	struct list_s *next;
-}
-
-list_t;
+} list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
