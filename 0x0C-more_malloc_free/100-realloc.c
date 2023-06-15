f9bfbe5d511cc11381da0b7bc3886aeac4337955
Attiyah02 <euphxric.melxnin@gmail.com>
2023-06-15 14:11:20 -0700

    _realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -22,6 +22,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		return (malloc(new_size));
 	}
+	if (new_size == old_size)
+		return (ptr);
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
@@ -37,6 +39,5 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		*(relloc + c) = clone[c];
 	}
 	free(ptr);
-
 	return (relloc);
 }
