9a4bceb4c9b2483a71f953584773812ed7a85477
Attiyah02 <euphxric.melxnin@gmail.com>
2023-06-15 13:47:58 -0700

    String_concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -35,26 +35,20 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 
 	if (n > size2)
-	{
-		n = size2;
-		p = malloc((size1 + n + 1) * sizeof(char));
-	}
-
+	n = size2;
+	p = malloc((size1 + n + 1) * sizeof(char));
 	if (p == NULL)
 	{
 		return (0);
 	}
-
 	for (c = 0; c < size1; c++)
 	{
 		p[c] = s1[c];
 	}
-
 	for (; c < (size1 + n); c++)
 	{
 		p[c] = s2[c - size1];
 	}
 	p[c] = '\0';
-	
 	return (p);
 }
