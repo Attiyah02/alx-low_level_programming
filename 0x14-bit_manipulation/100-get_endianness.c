457bcba04e7c89ea330da487cf3cadb59ec36c24
Attiyah02 <euphxric.melxnin@gmail.com>
2023-07-06 17:00:26 -0700

    Checks endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks endianess
+ *
+ * Return: big endian(0) little endian (1)
+ */
+
+int get_endianness(void)
+{
+	int nm = 1;
+	char *end = (char *)nm;
+
+	if (*end == 1)
+		return (1);
+
+	return (0);
+}
